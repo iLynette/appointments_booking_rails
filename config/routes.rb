4cfa629d2152d570ddd2e39859913cb81ded617b
iLynette <lynetteacholah@icloud.com>
2022-09-09 22:22:54 +0300

    Edit the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "rooms#index"
+  resources :rooms
 end
