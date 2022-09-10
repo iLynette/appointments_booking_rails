63d499195c74ff7fb5e9ae50814ac657f4b525e0
iLynette <lynetteacholah@icloud.com>
2022-09-10 18:44:52 +0300

    Define private method set room, find room by id

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,14 @@ class RoomsController < ApplicationController
     def index
         @rooms = Room.all
     end
+
+    def show
+        
+    end
+
+    private
+    
+    def set_room
+        @room = Room.find(params[:id])
+    end
 end
