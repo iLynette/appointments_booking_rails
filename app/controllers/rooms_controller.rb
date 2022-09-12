0298058a0c515238d0b0c1b0a8289d9a515aba48
iLynette <lynetteacholah@icloud.com>
2022-09-12 14:22:58 +0300

    Fix linter errors

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,32 +1,33 @@
 class RoomsController < ApplicationController
-    before_action :set_room, only: %i[show]
-    def index
-        @rooms = Room.all
-    end
+  before_action :set_room, only: %i[show]
+  def index
+    @rooms = Room.all
+  end
 
-    def show    
-    end
+  def show; end
 
-    def new
-        @room = Room.new
-    end
+  def new
+    @room = Room.new
+  end
 
-    def create
-      @room = Room.new(room_params)
-      respond_to do |format|
-        if @room.save
-            format.html { redirect_to room_url(@room), notice: "Room was created successfully" }
-        else
-            format.html { render :new, status: :unprocessable_entity}
-        end
+  def create
+    @room = Room.new(room_params)
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to room_url(@room), notice: 'Room was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
-    private
-    def set_room
-        @room = Room.find(params[:id])
-    end
+end
 
-    def room_params
-        params.require(:room).permit(:name, :capacity)
-    end
+    private
+
+def set_room
+  @room = Room.find(params[:id])
+end
+
+def room_params
+  params.require(:room).permit(:name, :capacity)
+end
