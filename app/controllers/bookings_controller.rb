1d99c0dd4b0deaeed78b29dcf156d3085a9ade6b
iLynette <lynetteacholah@icloud.com>
2022-09-14 13:52:09 +0300

    Define show and call back action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,15 @@
 class BookingsController < ApplicationController
-
+    before_action :set_booking, only: %i[show]
     def index
         @bookings = Booking.all
+    end
+
+    def show
+    end
+
+    private
+
+    def set_booking
+        @bookings = Booking.find(params[:id])
     end
 end
