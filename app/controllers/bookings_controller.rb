1cf074a83e78a7d278d05c7c46522df13db1658e
iLynette <lynetteacholah@icloud.com>
2022-09-14 19:30:19 +0300

    Create function and callbacks

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,35 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: %i[show]
+    before_action :set_booking, only: %i[show edit update destroy]
+  
     def index
-        @bookings = Booking.all
+      @bookings = Booking.all
     end
-
-    def show
-    end
-
+  
+    def show; end
+  
     def new
-        @bookings = Booking.new
+      @booking = Booking.new
     end
-
+  
+    def create
+      @booking = Booking.new(booking_params)
+      respond_to do |format|
+        if @booking.save
+          format.html { redirect_to booking_url(@booking), notice: 'Booking was created successfully' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
+  
     private
-
+  
     def set_booking
-        @bookings = Booking.find(params[:id])
+        @booking = Booking.find(params[:id])
     end
-end
+
+    def booking_params
+        params.require(:booking).permit(:room_id, :start, :end, :purpose)
+    end
+  end
+  
