8d64a255dfad1a6a72984e2d25da4a5c0596b7c9
iLynette <lynetteacholah@icloud.com>
2022-09-15 08:08:08 +0300

    Fix linter errors

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,54 +1,53 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: %i[show edit update destroy]
-  
-    def index
-      @bookings = Booking.all
-    end
-  
-    def show; end
-  
-    def new
-      @booking = Booking.new
-    end
-  
-    def create
-      @booking = Booking.new(booking_params)
-      respond_to do |format|
-        if @booking.save
-          format.html { redirect_to booking_url(@booking), notice: 'Booking was created successfully' }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def edit; end
-  
-    def update
-      respond_to do |format|
-        if @booking.update(booking_params)
-          format.html { redirect_to booking_url(@booking), notice: 'Booking was successfully updated.' }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def destroy
-      @booking.destroy
-      respond_to do |format|
-        format.html { redirect_to bookings_url, notice: 'booking was successfully destroyed.' }
-      end
-    end
-  
-    private
-  
-    def set_booking
-        @booking = Booking.find(params[:id])
-    end
+  before_action :set_booking, only: %i[show edit update destroy]
 
-    def booking_params
-        params.require(:booking).permit(:room_id, :start, :end, :purpose)
+  def index
+    @bookings = Booking.all
+  end
+
+  def show; end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    respond_to do |format|
+      if @booking.save
+        format.html { redirect_to booking_url(@booking), notice: 'Booking was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
-  
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @booking.update(booking_params)
+        format.html { redirect_to booking_url(@booking), notice: 'Booking was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    respond_to do |format|
+      format.html { redirect_to bookings_url, notice: 'booking was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:room_id, :start, :end, :purpose)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,3 @@
 class Booking < ApplicationRecord
-    belongs_to :room
+  belongs_to :room
 end
