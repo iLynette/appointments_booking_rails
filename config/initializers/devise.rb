bf4353c9fedeb15ef29eb1ae4fd8c1c83088cbf4
iLynette <lynetteacholah@icloud.com>
2022-09-15 18:28:27 +0300

    Add :turbo_stream as a navigational format

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,6 +38,7 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+config.navigational_formats = ['*/*', :html, :turbo_stream]
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
