58deca68e390d125f2a8bf5dc1795b3128b84b64
iLynette <lynetteacholah@icloud.com>
2022-09-17 15:53:49 +0300

    Authenticate user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   def index
     @bookings = Booking.all
@@ -48,6 +49,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:room_id, :start, :end, :purpose)
+    params.require(:booking).permit(:room_id, :start, :end, :purpose, :user_id)
   end
 end
