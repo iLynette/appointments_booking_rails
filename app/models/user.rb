7279c80ab1cc8e7cbf671967ab1bd77c8071a160
iLynette <lynetteacholah@icloud.com>
2022-09-18 13:50:10 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :bookings
+  has_many :bookings
 end
